b9026e510d8a05ecbd109dd7b46098ad99673a2e
James Lee <babun5209@gmail.com>
2017-03-06 14:56:26 -0800

    Add admin to users.

--- app/models/github.rb
+++ app/models/github.rb
@@ -1,6 +1,6 @@
 class Github
   def self.get_repos
-    response = RestClient.get("https://api.github.com/users/#{ENV['GITHUB_USERNAME']}/repos?sort=updated&access_token=#{ENV['GITHUB_ACCESS_TOKEN']}")
+    response = RestClient.get("https://api.github.com/users/#{ENV['GITHUB_USERNAME']}/starred?access_token=#{ENV['GITHUB_ACCESS_TOKEN']}")
     parsed_response = JSON.parse(response)
   end
 end
